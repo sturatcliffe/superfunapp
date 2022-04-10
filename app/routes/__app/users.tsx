dedf580f9eccc4a6b917ba9ae93881b5bbcd6268
Stu Ratcliffe <email@sturatcliffe.co.uk>
2022-04-10 11:42:57 +0100

    Use gravatar image instead of note icon

--- app/routes/__app/users.tsx
+++ app/routes/__app/users.tsx
@@ -1,27 +1,28 @@
 import { json, useLoaderData, Outlet, NavLink, redirect } from "remix";
 import type { LoaderFunction } from "remix";
 
-import { getUser, requireUserId } from "~/services/session.server";
+import { requireUser } from "~/services/session.server";
 import { getUsers } from "~/models/user.server";
+import { Md5 } from "md5-typescript";
 
 type LoaderData = {
   userListItems: Awaited<ReturnType<typeof getUsers>>;
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
-  await requireUserId(request);
-  const userListItems = await getUsers();
+  const user = await requireUser(request);
 
-  const user = await getUser(request);
   if (user && user.name == null) {
     return redirect("/profile");
   }
+
+  const userListItems = await getUsers();
 
   return json<LoaderData>({ userListItems });
 };
 
 export default function UsersPage() {
-  const data = useLoaderData() as LoaderData;
+  const data = useLoaderData<LoaderData>();
   return (
     <main className="flex flex-grow bg-white">
       <div className="w-80 border-r bg-gray-50">
@@ -30,11 +31,20 @@ export default function UsersPage() {
             <li key={user.id}>
               <NavLink
                 className={({ isActive }) =>
-                  `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
+                  `flex items-center border-b p-4 text-xl ${
+                    isActive ? "bg-white" : ""
+                  }`
                 }
                 to={user.id}
               >
-                📝 {user.name}
+                <img
+                  className="mr-4 rounded-full"
+                  src={`https://www.gravatar.com/avatar/${Md5.init(
+                    user.email
+                  )}?s=30&d=retro`}
+                  alt={user.name}
+                />
+                {user.name}
               </NavLink>
             </li>
           ))}
