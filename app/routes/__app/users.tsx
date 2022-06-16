566e80ba83672303a1ba5f2c632d2fc61e2d9919
Stu Ratcliffe <email@sturatcliffe.co.uk>
2022-06-16 23:06:23 +0100

    Hide user list on mobile by default

--- app/routes/__app/users.tsx
+++ app/routes/__app/users.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { json, useLoaderData, Outlet, NavLink, redirect } from "remix";
 import type { LoaderFunction } from "remix";
 
@@ -5,12 +6,14 @@ import { requireUser } from "~/services/session.server";
 import { getUsers } from "~/models/user.server";
 
 import Gravatar from "~/components/Gravatar";
+import { MenuIcon } from "@heroicons/react/outline";
 
 type LoaderData = {
+  userId: number;
   userListItems: Awaited<ReturnType<typeof getUsers>>;
 };
 
-export const loader: LoaderFunction = async ({ request }) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
   const user = await requireUser(request);
 
   if (user && user.name == null) {
@@ -19,34 +22,58 @@ export const loader: LoaderFunction = async ({ request }) => {
 
   const userListItems = await getUsers();
 
-  return json<LoaderData>({ userListItems });
+  return json<LoaderData>({ userId: parseInt(params.userId), userListItems });
 };
 
 export default function UsersPage() {
   const data = useLoaderData<LoaderData>();
+  const selectedUser = data.userListItems.find(
+    (x: any) => x.id === data.userId
+  );
+
+  const [open, setOpen] = useState(false);
+
+  const Users = () => {
+    return data.userListItems.map((user) => (
+      <li key={user.id}>
+        <NavLink
+          className={({ isActive }) =>
+            `flex items-center border-b p-4 text-lg md:text-xl ${
+              isActive ? "bg-white" : ""
+            }`
+          }
+          to={`${user.id}`}
+        >
+          <Gravatar name={user.name} email={user.email} className="mr-4" />
+          {user.name}
+        </NavLink>
+      </li>
+    ));
+  };
+
   return (
     <main className="flex h-full flex-col bg-white md:flex-row">
       <div className="w-full bg-gray-50 md:w-80 md:border-r">
-        <ol>
-          {data.userListItems.map((user) => (
-            <li key={user.id}>
-              <NavLink
-                className={({ isActive }) =>
-                  `flex items-center border-b p-4 text-xl ${
-                    isActive ? "bg-white" : ""
-                  }`
-                }
-                to={`${user.id}`}
-              >
-                <Gravatar
-                  name={user.name}
-                  email={user.email}
-                  className="mr-4"
-                />
-                {user.name}
-              </NavLink>
-            </li>
-          ))}
+        {/* <div className="flex items-center justify-between p-4 md:hidden">
+          <div className="flex items-center">
+            <Gravatar
+              name={selectedUser.name}
+              email={selectedUser.email}
+              className="mr-4"
+            />
+            {selectedUser.name}
+          </div>
+        </div> */}
+        <div className="flex justify-center py-2 md:hidden">
+          <MenuIcon onClick={() => setOpen(!open)} className="h-6 w-6" />
+        </div>
+        {open && (
+          <ol>
+            <Users />
+          </ol>
+        )}
+        <ol className="hidden md:block">
+          <Users />
         </ol>
       </div>
 
