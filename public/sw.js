7e41808a43064f883c3f544788c21a2c65afe0ea
Stu Ratcliffe <stu@ratcliffe.io>
2022-06-17 17:15:14 +0100

    Update start_url and cached routes

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@ var staticCacheName = "pwa";
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/", "/users", "/chat"]);
+      return cache.addAll(["/users", "/chat"]);
     })
   );
 });
