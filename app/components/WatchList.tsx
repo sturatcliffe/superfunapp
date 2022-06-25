abc6a96c33537c339d7a65c941285ba9ce53f525
Rohit Kamble <Rohit.x.Kamble@nielseniq.com>
2022-06-25 10:29:36 +0530

    fixWatchedUIStatus: fixed UI refresh issue

--- app/components/WatchList.tsx
+++ app/components/WatchList.tsx
@@ -22,6 +22,7 @@ const WatchList: FC<Props> = ({ items, currentUserId }) => {
           <div className="form-check flex items-center p-4">
             <input
               className="form-check-input float-left mt-1 mr-2 h-4 w-4 cursor-pointer appearance-none rounded-sm border border-gray-300 bg-white bg-contain bg-center bg-no-repeat align-top transition duration-200 checked:border-blue-600 checked:bg-blue-600 focus:outline-none"
+              checked={showAll}
               onChange={() => setShowAll(!showAll)}
               type="checkbox"
             />
