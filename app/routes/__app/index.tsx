0fb45e458568f62d8f05312699b218a863f53907
Stu Ratcliffe <stu@ratcliffe.io>
2022-06-28 15:59:42 +0100

    Add comment to allow a PR

--- app/routes/__app/index.tsx
+++ app/routes/__app/index.tsx
@@ -2,6 +2,7 @@ import { LoaderFunction, redirect } from "remix";
 import { getUserId } from "~/services/session.server";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
+  //TODO : fix the redirect issue here
   const userId = await getUserId(request);
   return redirect(userId ? `/users/${userId}` : "/login");
 };
