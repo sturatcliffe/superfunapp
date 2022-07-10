5e780a237daf948757446decc93869ad1dea1f36
Stu Ratcliffe <stu@ratcliffe.io>
2022-07-10 13:57:20 +0100

    Add proper responsive navbar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,21 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Link, Form, useLocation } from "remix";
-import { LogoutIcon } from "@heroicons/react/outline";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 import Pusher from "pusher-js";
 
 import { useMatchesData, useUser } from "~/utils";
+import Gravatar from "./Gravatar";
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const navigation = [
+  { name: "Dashboard", href: "/" },
+  { name: "Users", href: "/users" },
+  { name: "Chat", href: "/chat" },
+];
 
 export default function Header() {
   const user = useUser();
@@ -37,35 +49,216 @@ export default function Header() {
   }, [location]);
 
   return (
-    <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
-      <h1>
-        <Link to={`/users/${user.id}`}>
-          <img className="w-24 md:w-48" src="/logo.png" alt="SuperFunApp" />
-        </Link>
-      </h1>
-      <Link to="/profile" className="text-sm md:text-base">
-        {user.email}
-      </Link>
-      <div className="flex items-center">
-        <Link to="/chat" className="relative text-sm md:text-base">
-          Chat{" "}
-          {hasUnreadMessages && (
-            <span className="absolute -top-1 -right-2 flex h-1 w-1 md:h-2 md:w-2">
-              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
-              <span className="relative inline-flex h-1 w-1 rounded-full bg-red-500 md:h-2 md:w-2"></span>
-            </span>
-          )}
-        </Link>
-        <Form action="/logout" method="post" className="ml-4">
-          <button
-            type="submit"
-            className="flex items-center rounded bg-slate-600 p-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600 md:py-2 md:px-4"
-          >
-            <LogoutIcon className="h-4 w-4" />
-            <span className="hidden md:ml-2 md:inline">Logout</span>
-          </button>
-        </Form>
-      </div>
-    </header>
+    <Disclosure as="header" className="bg-slate-800">
+      {({ open }) => (
+        <>
+          <div className="px-4">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <Link to="/">
+                    <img
+                      className="w-36 md:w-48"
+                      src="/logo.png"
+                      alt="SuperFunApp"
+                    />
+                  </Link>
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        to={item.href}
+                        className={classNames(
+                          location.pathname === item.href
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "rounded-md px-3 py-2 text-sm font-medium"
+                        )}
+                        aria-current={
+                          location.pathname === item.href ? "page" : undefined
+                        }
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <Menu as="div" className="relative z-10">
+                  <Menu.Button className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none">
+                    <span className="sr-only">View notifications</span>
+                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    {hasUnreadMessages && (
+                      <span className="absolute top-0 right-0 flex h-1 w-1 md:h-2 md:w-2">
+                        <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
+                        <span className="relative inline-flex h-1 w-1 rounded-full bg-red-500 md:h-2 md:w-2"></span>
+                      </span>
+                    )}
+                  </Menu.Button>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <>
+                            {hasUnreadMessages && (
+                              <Link
+                                to="/chat"
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                              >
+                                You have {data?.unreadMessageCount} unread chat
+                                messages
+                              </Link>
+                            )}
+                            {!hasUnreadMessages && (
+                              <Link
+                                to={location.pathname}
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                              >
+                                No new notifications
+                              </Link>
+                            )}
+                          </>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative z-10 ml-3">
+                  <div>
+                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none">
+                      <span className="sr-only">Open user menu</span>
+                      <Gravatar email={user.email} />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            to="/profile"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Your Profile
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            to={`/users/${user.id}`}
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Your list
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            to="/settings"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Settings
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Form
+                            action="/logout"
+                            method="post"
+                            className="w-full"
+                          >
+                            <button
+                              type="submit"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block w-full px-4 py-2 text-left text-sm text-gray-700"
+                              )}
+                            >
+                              Sign out
+                            </button>
+                          </Form>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as={Link}
+                  to={item.href}
+                  className={classNames(
+                    location.pathname === item.href
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )}
+                  aria-current={
+                    location.pathname === item.href ? "page" : undefined
+                  }
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 }
